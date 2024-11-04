2efcdef42cd7b0ac11966ee889eb7be422c80c1b
pvk05 <philip.v.knudsen@gmail.com>
2024-11-04 16:08:47 +0100

    added functionality for storing multiple themes

--- app/components/Login/LoginButton.js
+++ app/components/Login/LoginButton.js
@@ -21,7 +21,6 @@ import { Person } from "@mui/icons-material";
 import { cybTheme } from "../themeCYB";
 import { useRouter } from "next/navigation";
 
-import { mdiGhost } from '@mdi/js';
 import Icon from "@mdi/react";
 import Link from "next/link";
 // import { useEffect, useState } from "react";
@@ -76,7 +75,7 @@ export default function LoginButton(props) {
                 {session.status == "authenticated" ? (
                   <Icon
                     alt="Image of user"
-                    path={mdiGhost}
+                    path={cybTheme.profilePic}
                     color={cybTheme.palette.background.main}
                   />
                 ) : (
@@ -124,7 +123,7 @@ export default function LoginButton(props) {
               {session.status == "authenticated" ? (
                 <Avatar alt="Image of user" sx={{ ...avatarProps }}>
                   <Icon
-                    path={mdiGhost}
+                    path={cybTheme.profilePic}
                     color={cybTheme.palette.background.main}
                   />
                 </Avatar>
--- app/components/themeCYB.js
+++ app/components/themeCYB.js
@@ -1,20 +1,44 @@
 
 import { createTheme } from "@mui/material";
+import { mdiGhost, mdiPenguin } from '@mdi/js';
 
-export const cybTheme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#E66C2C",
+const themes = {
+  main: {
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#d2a30e",
+      },
+      secondary: {
+        main: "#ac0bce",
+      },
+      warning: {
+        main: "#ffe358",
+      },
+      background: {
+        // paper: "#181817",
+      },
     },
-    secondary: {
-      main: "#ac0bce",
-    },
-    warning: {
-      main: "#ffe358",
-    },
-    background: {
-      // paper: "#181817",
-    },
+    profilePic: mdiPenguin
   },
-});
+  halloween: {
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#E66C2C",
+      },
+      secondary: {
+        main: "#ac0bce",
+      },
+      warning: {
+        main: "#ffe358",
+      },
+      background: {
+        // paper: "#181817",
+      },
+    },
+    profilePic: mdiGhost
+  }
+}
+
+export const cybTheme = createTheme(themes["main"]);
